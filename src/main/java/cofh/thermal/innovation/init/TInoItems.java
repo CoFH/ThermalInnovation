48a728feea3d5e8486d0cf2bab3a2130132eddfe
King Lemming <kinglemming@gmail.com>
2021-08-25 02:14:34 -0700

    Mod tagging. :)
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermal/innovation/init/TInoItems.java
+++ src/main/java/cofh/thermal/innovation/init/TInoItems.java
@@ -4,6 +4,8 @@ import cofh.thermal.innovation.item.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
+import static cofh.lib.util.constants.Constants.ID_THERMAL_EXPANSION;
+import static cofh.lib.util.constants.Constants.ID_THERMAL_INNOVATION;
 import static cofh.thermal.core.util.RegistrationHelper.registerItem;
 import static cofh.thermal.lib.common.ThermalItemGroups.THERMAL_TOOLS;
 
@@ -29,16 +31,16 @@ public class TInoItems {
         int fluid = 4000;
         int arrows = 80;
 
-        registerItem("flux_drill", () -> new RFDrillItem(new Item.Properties().maxStackSize(1).group(group), energy, xfer));
-        registerItem("flux_saw", () -> new RFSawItem(new Item.Properties().maxStackSize(1).group(group), energy, xfer));
-        // registerItem("flux_pump", () -> new RFPumpItem(new Item.Properties().maxStackSize(1).group(group), energy, xfer));
-        registerItem("flux_capacitor", () -> new RFCapacitorItem(new Item.Properties().maxStackSize(1).group(group), energy * 10, xfer));
-        registerItem("flux_magnet", () -> new RFMagnetItem(new Item.Properties().maxStackSize(1).group(group), energy, xfer));
+        registerItem("flux_drill", () -> new RFDrillItem(new Item.Properties().maxStackSize(1).group(group), energy, xfer).setModId(ID_THERMAL_INNOVATION));
+        registerItem("flux_saw", () -> new RFSawItem(new Item.Properties().maxStackSize(1).group(group), energy, xfer).setModId(ID_THERMAL_INNOVATION));
+        // registerItem("flux_pump", () -> new RFPumpItem(new Item.Properties().maxStackSize(1).group(group), energy, xfer).setModId(ID_THERMAL_INNOVATION));
+        registerItem("flux_capacitor", () -> new RFCapacitorItem(new Item.Properties().maxStackSize(1).group(group), energy * 10, xfer).setModId(ID_THERMAL_INNOVATION));
+        registerItem("flux_magnet", () -> new RFMagnetItem(new Item.Properties().maxStackSize(1).group(group), energy, xfer).setModId(ID_THERMAL_INNOVATION));
 
-        registerItem("fluid_reservoir", () -> new FluidReservoirItem(new Item.Properties().maxStackSize(1).group(group), fluid * 4));
+        registerItem("fluid_reservoir", () -> new FluidReservoirItem(new Item.Properties().maxStackSize(1).group(group), fluid * 4).setModId(ID_THERMAL_INNOVATION));
 
-        registerItem("potion_infuser", () -> new PotionInfuserItem(new Item.Properties().maxStackSize(1).group(group), fluid));
-        registerItem("potion_quiver", () -> new PotionQuiverItem(new Item.Properties().maxStackSize(1).group(group), fluid, arrows));
+        registerItem("potion_infuser", () -> new PotionInfuserItem(new Item.Properties().maxStackSize(1).group(group), fluid).setModId(ID_THERMAL_INNOVATION));
+        registerItem("potion_quiver", () -> new PotionQuiverItem(new Item.Properties().maxStackSize(1).group(group), fluid, arrows).setModId(ID_THERMAL_INNOVATION));
     }
 
     private static void registerArmor() {
