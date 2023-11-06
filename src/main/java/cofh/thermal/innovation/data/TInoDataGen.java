637fed344029ec99f1d3f53f9f12777ece378386
King Lemming <kinglemming@gmail.com>
2023-11-05 23:28:33 -0800

    DataGen updates.

--- src/main/java/cofh/thermal/innovation/data/TInoDataGen.java
+++ src/main/java/cofh/thermal/innovation/data/TInoDataGen.java
@@ -1,30 +1,31 @@
-//package cofh.thermal.innovation.data;
-//
-//import net.minecraft.data.DataGenerator;
-//import net.minecraft.data.PackOutput;
-//import net.minecraftforge.common.data.ExistingFileHelper;
-//import net.minecraftforge.data.event.GatherDataEvent;
-//import net.minecraftforge.eventbus.api.SubscribeEvent;
-//import net.minecraftforge.fml.common.Mod;
-//
-//import static cofh.lib.util.constants.ModIds.ID_THERMAL_INNOVATION;
-//
-//@Mod.EventBusSubscriber (bus = Mod.EventBusSubscriber.Bus.MOD, modid = ID_THERMAL_INNOVATION)
-//public class TInoDataGen {
-//
-//    @SubscribeEvent
-//    public static void gatherData(final GatherDataEvent event) {
-//
-//        DataGenerator gen = event.getGenerator();
-//        PackOutput pOutput = gen.getPackOutput();
-//        ExistingFileHelper exFileHelper = event.getExistingFileHelper();
-//
-//        TInoTagsProvider.Block blockTags = new TInoTagsProvider.Block(pOutput, event.getLookupProvider(), exFileHelper);
-//
-//        gen.addProvider(event.includeServer(), blockTags);
-//        gen.addProvider(event.includeServer(), new TInoTagsProvider.Item(pOutput, event.getLookupProvider(), blockTags.contentsGetter(), exFileHelper));
-//
-//        gen.addProvider(event.includeServer(), new TInoRecipeProvider(gen));
-//    }
-//
-//}
+package cofh.thermal.innovation.data;
+
+import cofh.thermal.innovation.data.providers.TInoRecipeProvider;
+import cofh.thermal.innovation.data.providers.TInoTagsProvider;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import static cofh.lib.util.constants.ModIds.ID_THERMAL_INNOVATION;
+
+@Mod.EventBusSubscriber (bus = Mod.EventBusSubscriber.Bus.MOD, modid = ID_THERMAL_INNOVATION)
+public class TInoDataGen {
+
+    @SubscribeEvent
+    public static void gatherData(final GatherDataEvent event) {
+
+        DataGenerator gen = event.getGenerator();
+        PackOutput output = gen.getPackOutput();
+        ExistingFileHelper exFileHelper = event.getExistingFileHelper();
+
+        TInoTagsProvider.Block blockTags = new TInoTagsProvider.Block(output, event.getLookupProvider(), exFileHelper);
+        gen.addProvider(event.includeServer(), blockTags);
+        gen.addProvider(event.includeServer(), new TInoTagsProvider.Item(output, event.getLookupProvider(), blockTags.contentsGetter(), exFileHelper));
+
+        gen.addProvider(event.includeServer(), new TInoRecipeProvider(output));
+    }
+
+}
