b5a4feb9e478034a7514bac6a131ce0ecbb263ee
King Lemming <kinglemming@gmail.com>
2023-12-21 20:13:54 -0800

    Creative Tab Sorting.

--- src/main/java/cofh/thermal/innovation/init/registries/TInoItems.java
+++ src/main/java/cofh/thermal/innovation/init/registries/TInoItems.java
@@ -34,17 +34,17 @@ public class TInoItems {
         int fluid = 4000;
         int arrows = 80;
 
-        toolsTab(registerItem(ID_FLUX_DRILL, () -> new RFDrillItem(new Item.Properties().stacksTo(1), energy, xfer).setModId(ID_THERMAL_INNOVATION)));
-        toolsTab(registerItem(ID_FLUX_SAW, () -> new RFSawItem(new Item.Properties().stacksTo(1), energy, xfer).setModId(ID_THERMAL_INNOVATION)));
+        toolsTab(10, registerItem(ID_FLUX_DRILL, () -> new RFDrillItem(new Item.Properties().stacksTo(1), energy, xfer).setModId(ID_THERMAL_INNOVATION)));
+        toolsTab(10, registerItem(ID_FLUX_SAW, () -> new RFSawItem(new Item.Properties().stacksTo(1), energy, xfer).setModId(ID_THERMAL_INNOVATION)));
         // registerItem(ID_FLUX_PUMP, () -> new RFPumpItem(new Item.Properties().maxStackSize(1).group(group), energy, xfer).setModId(ID_THERMAL_INNOVATION));
-        toolsTab(registerItem(ID_FLUX_CAPACITOR, () -> new RFCapacitorItem(new Item.Properties().stacksTo(1), energy * 10, xfer).setModId(ID_THERMAL_INNOVATION)));
-        toolsTab(registerItem(ID_FLUX_MAGNET, () -> new RFMagnetItem(new Item.Properties().stacksTo(1), energy, xfer).setModId(ID_THERMAL_INNOVATION)));
+        toolsTab(10, registerItem(ID_FLUX_CAPACITOR, () -> new RFCapacitorItem(new Item.Properties().stacksTo(1), energy * 10, xfer).setModId(ID_THERMAL_INNOVATION)));
+        toolsTab(10, registerItem(ID_FLUX_MAGNET, () -> new RFMagnetItem(new Item.Properties().stacksTo(1), energy, xfer).setModId(ID_THERMAL_INNOVATION)));
         //registerItem(ID_FLUX_GRAPPLE, () -> new RFGrappleItem(new Item.Properties().stacksTo(1), energy, xfer).setModId(ID_THERMAL_INNOVATION));
 
-        toolsTab(registerItem(ID_FLUID_RESERVOIR, () -> new FluidReservoirItem(new Item.Properties().stacksTo(1), fluid * 4).setModId(ID_THERMAL_INNOVATION)));
+        toolsTab(10, registerItem(ID_FLUID_RESERVOIR, () -> new FluidReservoirItem(new Item.Properties().stacksTo(1), fluid * 4).setModId(ID_THERMAL_INNOVATION)));
 
-        toolsTab(registerItem(ID_POTION_INFUSER, () -> new PotionInfuserItem(new Item.Properties().stacksTo(1), fluid).setModId(ID_THERMAL_INNOVATION)));
-        toolsTab(registerItem(ID_POTION_QUIVER, () -> new PotionQuiverItem(new Item.Properties().stacksTo(1), fluid, arrows).setModId(ID_THERMAL_INNOVATION)));
+        toolsTab(10, registerItem(ID_POTION_INFUSER, () -> new PotionInfuserItem(new Item.Properties().stacksTo(1), fluid).setModId(ID_THERMAL_INNOVATION)));
+        toolsTab(10, registerItem(ID_POTION_QUIVER, () -> new PotionQuiverItem(new Item.Properties().stacksTo(1), fluid, arrows).setModId(ID_THERMAL_INNOVATION)));
     }
 
     private static void registerArmor() {
